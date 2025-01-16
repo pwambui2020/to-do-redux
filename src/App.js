d67afdad3f2a2b26d4be783520cfccafd969b307
pwwambui <pwwambui@safaricom.co.ke>
2025-01-16 15:41:35 +0300

    added the title and pulled the display and todo components to the UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+
 import './App.css';
+import DisplayTodos from './components/DisplayTodo';
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is a TO Do List App</h1>
+      <h1>
+        This is a To Do App
+      </h1>
+      <div>
+        <Todos />
+        <DisplayTodos />
+      </div>
     </div>
   );
 }
